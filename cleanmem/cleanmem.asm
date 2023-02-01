4a7e6b1c84c17a2072cd4d20feed7b43facaad30
ajw-accenture <ajw-accenture@users.noreply.github.com>
2023-02-01 08:56:27 -0600

    refactor: fix bug
    * 0x00 was not being zeroed out; flipped loop so that the decrement occurs first then the zero out happens
    * 0xFF has to be directly set to zero, unfortunately.

--- cleanmem/cleanmem.asm
+++ cleanmem/cleanmem.asm
@@ -12,10 +12,11 @@ Start:                  ; This label is literally $F000 in memory
 ; Clear Page Zero Region ($00 to $FF; includes the RAM and TIA registers)
     lda #0              ; Put zero in the A register
     ldx #$FF            ; Put 0xFF in the X register
+    sta $FF             ; Zeros memory 0xFF before the loop starts
 
 MemClearLoop;
-    sta $0,X            ; Store the value of register A into $0 + value of register X
     dex                 ; Decrement X
+    sta $0,X            ; Store the value of register A into $0 + value of register X - does not change processor flags
     bne MemClearLoop    ; Branch to MemClearLoop if X is not zero (Z flag != 0)
 
 ; Close out ROM cartridge (This is required)
@@ -23,3 +24,4 @@ MemClearLoop;
     .word Start         ; Set where the program starts
     .word Start         ; Set the interrupt vector at $FFFE ($FFFC + 2)
 
+; dasm cleanmem.asm -f3 -v0 -ocart.bin
